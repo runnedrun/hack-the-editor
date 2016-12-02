b3322b40ca71524b50ba0c0d2b02e785ea754a39
David Gaynor <runnedrun@gmail.com>
2016-12-02 08:06:48 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -8,4 +8,5 @@ var allDivsInTheEditor = editor.find("div")
 // Now try to
 // 1. add a button to the topbar
 // 2. change the title above the file list
-// 3. change the borders 
+// 3. change some borders in the editor
+// 4. Make all this happen when a button is clicked
