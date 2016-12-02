da8fdcce2a58dd48674add244bfe96e218b9d661
David Gaynor <runnedrun@gmail.com>
2016-12-02 08:02:43 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var editor = $(parent.document.body)
-var topbar = $(parent.document.body)
+var topbar = $(parent.parent.document.body).find("#topbar")
+
 var allDivsInTheEditor = editor.find("div")
 
 // Now try to
